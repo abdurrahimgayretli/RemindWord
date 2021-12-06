5e31e193b14e43bacd08f846af5b64c755a177e6
xasher <abdurrahimgayretli@gmail.com>
2021-12-06 09:38:49 +0300

    notify commit

--- app/src/main/java/com/example/notificationdictionaryapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationdictionaryapp/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.notificationdictionaryapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.notificationdictionaryapp.viewmodel.WordViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val CHANNEL_ID = "channel_id_notify_01"
+    private  val notificationId = 101
+    private lateinit var mWordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        button.setOnClickListener { sendNotification() }
+        mWordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
 
         setupActionBarWithNavController(findNavController(R.id.fragment))
     }
@@ -20,8 +37,30 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
-
-
+    private  fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private  fun sendNotification(){
+        mWordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Notify Title")
+            .setContentText("text")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId,builder.build())
+        }
+    }
 }
 
 
